f60a441faae399fa592a7c50af652373361f2b08
benjamin almiron <almironbenjamin2001@gmail.com>
2025-03-13 16:52:53 -0300

    .

--- src/dirname.js
+++ src/dirname.js
@@ -1,14 +1,14 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-// Obtén la ruta del archivo actual (dirname.js)
+
 const __filename = fileURLToPath(import.meta.url);
 
-// Obtén la carpeta donde está el archivo
+
 const __dirname = dirname(__filename);
 
-// Subir dos directorios para llegar a la raíz del proyecto
-const rootDir = dirname(__dirname);  // Esto sube un nivel al directorio "src"
+
+const rootDir = dirname(__dirname);  
 
 export default rootDir;
 
--- src/middlewares/passport.auth.js
+++ src/middlewares/passport.auth.js
@@ -3,16 +3,16 @@ import passport from 'passport';
 const passportAuth = (strategy) => {
     return async (req, res, next) => {
         passport.authenticate(strategy, { session: false }, function(error, user, info) {
-            if (error) return next(error);  // Si hay un error en la autenticación
+            if (error) return next(error);  
             if (!user) {
-                // Si no se encuentra al usuario, respondemos con un error 401
+                
                 return res.status(401).send({
                     error: info && info.message ? info.message : "No autorizado"
                 });
             }
-            req.user = user;  // Si el usuario es válido, lo asignamos a la petición
-            next();  // Continuamos con la siguiente función de middleware
-        })(req, res, next);  // Llamamos al método `authenticate` de Passport
+            req.user = user;  
+            next();  
+        })(req, res, next);  
     };
 };
 
